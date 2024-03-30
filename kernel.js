325b5adc82b31ae025756674cb23eb2297944467
tayoky <139649656+tayoky@users.noreply.github.com>
2024-03-30 14:37:40 +0100

    new test

--- kernel.js
+++ kernel.js
@@ -1,22 +1,29 @@
 const FPS = 30;
-setInterval(udapte,1000/FPS);
+setInterval(update, 1000 / FPS);
+let windows = document.getElementsByClassName("window");
 
-const selectwindow = -1;
-document.onmouseup = releasemouse;
+let selectwindow = -1;
+let selectwindowOffsetX = 0;
+let selectwindowOffsetY = 0;
 
+document.onmouseup = releaseMouse;
 
-function toolbarclick(Select){
+function toolbarClick(Select) {
+    tevent = window.Event;
     selectwindow = Select;
+    selectwindowOffsetX = Tevent.pageX - windows[Select].getAttribute("left");
+    selectwindowOffsetY = Tevent.pageY - windows[Select].getAttribute("top");
 }
 
-function releasemouse(){
+function releaseMouse() {
     selectwindow = -1;
 }
 
-function udapte(){
-    let windows = document.getElementsByClassName("window");
+function update() {
+    windows = document.getElementsByClassName("window");
 
     for (let index = 0; index < windows.length; index++) {
-        index[index]
+        // Faites quelque chose avec chaque élément de la liste "windows"
+        // Par exemple : windows[index].style.backgroundColor = "red";
     }
 }
