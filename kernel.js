8fad85ac23bef1d9bd85d331c7291abaac397db7
tayoky <139649656+tayoky@users.noreply.github.com>
2024-03-30 15:10:17 +0100

    new version

--- kernel.js
+++ kernel.js
@@ -1,5 +1,7 @@
 const FPS = 30;
 setInterval(update, 1000 / FPS);
+
+// Récupérez vos éléments de fenêtre ici (par exemple, par ID ou classe)
 let windows = document.getElementsByClassName("window");
 
 let selectwindow = -1;
@@ -10,10 +12,11 @@ document.onmouseup = releaseMouse;
 window.onmousemove = mouseMove;
 
 function toolbarClick(Select) {
-    let tevent = window.event;
+    // Utilisez l'objet event passé en argument
+    let event = window.event;
     selectwindow = Select;
-    selectwindowOffsetX = tevent.pageX - windows[Select].getAttribute("left");
-    selectwindowOffsetY = tevent.pageY - windows[Select].getAttribute("top");
+    selectwindowOffsetX = event.pageX - windows[Select].getBoundingClientRect().left;
+    selectwindowOffsetY = event.pageY - windows[Select].getBoundingClientRect().top;
 }
 
 function releaseMouse() {
@@ -21,15 +24,13 @@ function releaseMouse() {
 }
 
 function update() {
-    windows = document.getElementsByClassName("window");
-
-
+    // Mettez à jour l'état de vos fenêtres ici
+    // Par exemple, redimensionnez-les, mettez à jour leur contenu, etc.
 }
 
-function mouseMove(event){
-    if(selectwindow !== -1){
-        windows[selectwindow].setAttribute("left",event.x - selectwindowOffsetX);
-        windows[selectwindow].setAttribute("top" ,event.y - selectwindowOffsetY);
+function mouseMove(event) {
+    if (selectwindow !== -1) {
+        windows[selectwindow].style.left = event.x - selectwindowOffsetX + "px";
+        windows[selectwindow].style.top = event.y - selectwindowOffsetY + "px";
     }
 }
-
