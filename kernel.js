02959e23ab6e9001ff9abfc346125c326956a639
tayoky <139649656+tayoky@users.noreply.github.com>
2024-03-30 10:56:43 +0100

    select tools bar

--- kernel.js
+++ kernel.js
@@ -1,6 +1,18 @@
 const FPS = 30;
 setInterval(udapte,1000/FPS);
 
+const selectwindow = -1;
+document.onmouseup = releasemouse;
+
+
+function toolbarclick(Select){
+    selectwindow = Select;
+}
+
+function releasemouse(){
+    selectwindow = -1;
+}
+
 function udapte(){
     let windows = document.getElementsByClassName("window");
 
